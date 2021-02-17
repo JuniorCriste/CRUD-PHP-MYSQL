7f4314f1a4fa3b2d75afa027a016c3dd266d449a
Junior Criste <informaticode@gmail.com>
2021-02-17 14:18:08 -0300

    structure layout

--- crud.php
+++ crud.php
@@ -17,20 +17,20 @@
 <div id="table">
     <img src="img/structure/basetb.png">
     </div>
-</div>
 
 <div id="buttons">
-    <img src="img/structure/bntadd.png">
-    <img src="img/structure/bntsec.png">
-    <img src="img/structure/bntmod.png">
-    <img src="img/structure/bntdel.png">
+    <img src="img/structure/bntadd.png"> <br /> <br />
+    <img src="img/structure/bntsec.png"> <br /> <br />
+    <img src="img/structure/bntmod.png"> <br /> <br />
+    <img src="img/structure/bntdel.png"> 
     </div>
+</div>
 
 <div id="bottom">
-    <img src="img/structure/inst.png">
-    <img src="img/structure/mail.png">
-    <img src="img/structure/whtpp.png">
-    <img src="img/structure/git.png">
+    <a href="https://www.instagram.com/informaticode/" target="_blank"><img src="img/structure/inst.png"></a>
+    <a href="mailto:informaticode@gmail.com" target="_blank"><img src="img/structure/mail.png"></a>
+    <a href="https://api.whatsapp.com/send?phone=5527988334621" target="_blank"><img src="img/structure/whtpp.png"></a>
+    <a href="https://github.com/JuniorCriste/CRUD-PHP-MYSQL" target="_blank"><img src="img/structure/git.png"></a>
     </div>
 
 
