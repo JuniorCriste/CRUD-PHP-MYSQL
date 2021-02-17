c2910fa002c8e4bd5ad2730ebabf4ca679c82bac
Junior Criste <informaticode@gmail.com>
2021-02-17 13:39:02 -0300

    structure layout base

---
+++ crud.php
@@ -0,0 +1,40 @@
+<DOCTYPE html>
+<html lang="pt-br">
+<head>
+<title>CRUD em PHP e MySQL</title>
+<link href="src/styles/crud.css" rel="stylesheet">
+<meta charset="UTF-8">
+<meta content='width=device-width, minimum-scale=1.0, maximum-scale=1.0' name='viewport'/>
+<link rel="shortcut icon" type="image/png" href="img/icon.png"/>
+
+</head>
+<body>
+<div id="top">
+    <img src="img/structure/top.png">
+    </div>
+
+<div id="main">
+<div id="table">
+    <img src="img/structure/basetb.png">
+    </div>
+</div>
+
+<div id="buttons">
+    <img src="img/structure/bntadd.png">
+    <img src="img/structure/bntsec.png">
+    <img src="img/structure/bntmod.png">
+    <img src="img/structure/bntdel.png">
+    </div>
+
+<div id="bottom">
+    <img src="img/structure/inst.png">
+    <img src="img/structure/mail.png">
+    <img src="img/structure/whtpp.png">
+    <img src="img/structure/git.png">
+    </div>
+
+
+
+
+</body>
+</html>
