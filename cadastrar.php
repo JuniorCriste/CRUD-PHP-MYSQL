442f96b64b3961c5872840867106693833c85947
JuniorCriste <informaticode@gmail.com>
2022-06-18 20:35:40 -0300

    1.0

---
+++ cadastrar.php
@@ -0,0 +1,11 @@
+<?php
+    echo"Hello World"    
+    }
+>
+<form method="post">
+    <input type="text" name="nome">
+    <input type="text" name="sobrenome">
+    <input type="text" name="email">
+    <input type="text" name="celular">
+    <input type="submit" name="Enviar">
+</form>
