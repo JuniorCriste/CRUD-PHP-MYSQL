108d2b141343d4570fe0c94aa6c188be721ca930
JuniorCriste <informaticode@gmail.com>
2022-06-18 23:15:59 -0300

    1.0

--- crud.php
+++ crud.php
@@ -1,6 +1,6 @@
 <?php
 
-include "/database/conecta.php";
+include "database/conecta.php";
 $buscar_cadastros = "SELECT * FROM crud";
 $query_cadastros = mysqli_query($conx, $buscar_cadastros);
 ?>
@@ -25,23 +25,6 @@ $query_cadastros = mysqli_query($conx, $buscar_cadastros);
     <img src="img/structure/basetb.png">
 <br/> 
 
-</div>
-
-
-<div id="buttons">
-    <a href="src/add.php"><img src="img/structure/bntadd.png"> </a> <br /> <br />
-    <img src="img/structure/bntsec.png"> <br /> <br />
-    <img src="img/structure/bntmod.png"> <br /> <br />
-    <img src="img/structure/bntdel.png"> 
-    </div>
-</div>
-
-<div id="bottom">
-    <a href="https://www.instagram.com/informaticode/" target="_blank"><img src="img/structure/inst.png"></a>
-    <a href="mailto:informaticode@gmail.com" target="_blank"><img src="img/structure/mail.png"></a>
-    <a href="https://api.whatsapp.com/send?phone=5527988334621" target="_blank"><img src="img/structure/whtpp.png"></a>
-    <a href="https://github.com/JuniorCriste/CRUD-PHP-MYSQL" target="_blank"><img src="img/structure/git.png"></a>
-    </div>
 
 <table>   
        <?php
@@ -66,6 +49,23 @@ $query_cadastros = mysqli_query($conx, $buscar_cadastros);
 
 
 
+</div>
+
+
+<div id="buttons">
+    <a href="src/add.php"><img src="img/structure/bntadd.png"> </a> <br /> <br />
+    <img src="img/structure/bntsec.png"> <br /> <br />
+    <img src="img/structure/bntmod.png"> <br /> <br />
+    <img src="img/structure/bntdel.png"> 
+    </div>
+</div>
+
+<div id="bottom">
+    <a href="https://www.instagram.com/informaticode/" target="_blank"><img src="img/structure/inst.png"></a>
+    <a href="mailto:informaticode@gmail.com" target="_blank"><img src="img/structure/mail.png"></a>
+    <a href="https://api.whatsapp.com/send?phone=5527988334621" target="_blank"><img src="img/structure/whtpp.png"></a>
+    <a href="https://github.com/JuniorCriste/CRUD-PHP-MYSQL" target="_blank"><img src="img/structure/git.png"></a>
+    </div>
 
 </body>
 </html>
